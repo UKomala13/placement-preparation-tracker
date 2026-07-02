0c0b47ceaf48c70914a3f6e9c7bd2b6ea859e792
UKomala13 <komalaudayagiri@gmail.com>
2026-07-02 10:41:25 +0530

    Complete Stage 1: CRUD, Search, Dynamic Filtering

---
+++ public/script.js
@@ -0,0 +1,211 @@
+const form = document.getElementById("topicForm");
+const topicscontainer = document.getElementById("topicsContainer")
+const submitBtn = document.getElementById("submit");
+const message = document.getElementById("message");
+const searchInput = document.getElementById("searchInput");
+const categoryFilter = document.getElementById("categoryFilter");
+
+const category = document.getElementById("category");
+const topicName = document.getElementById("topicName");
+const status = document.getElementById("status");
+const questionsSolved = document.getElementById("questionsSolved");
+
+let topics = [];
+let editTopicId = null;
+
+function showMessage(text, type) {
+
+    message.textContent = text;
+
+    if (type === "success") {
+        message.style.color = "green";
+    }
+    else if (type === "error") {
+        message.style.color = "red";
+    }
+
+    setTimeout(() => {
+        message.textContent = "";
+    }, 3000);
+}
+
+function populateCategoryFilter() {  //to fill the dropdown
+
+    categoryFilter.innerHTML = "";
+
+    const allOption = document.createElement("option");
+    allOption.value = "All";
+    allOption.textContent = "All Categories";
+    categoryFilter.append(allOption);
+
+    const categories = topics.map(topic => topic.category);
+    const uniqueCategories = [...new Set(categories)];   //array->set->array
+
+    uniqueCategories.forEach(category => {
+        const option = document.createElement("option");
+        option.value = category;
+        option.textContent = category;
+        categoryFilter.append(option);
+    })
+
+}
+
+function applyFilters() {
+    let filteredTopics = topics;
+    const searchText = searchInput.value.toLowerCase();
+    const selectedCategory = categoryFilter.value;
+
+    if (searchText !== "") {
+        filteredTopics = filteredTopics.filter(topic =>
+            topic.topic_name.toLowerCase().includes(searchText)
+        );
+    }
+    if (selectedCategory !== "All") {
+        filteredTopics = filteredTopics.filter(topic =>
+            topic.category === selectedCategory
+        )
+    }
+    topicscontainer.innerHTML="";
+    if (filteredTopics.length === 0) {
+        topicscontainer.innerHTML = "<h2> No topics found </h2>";
+        return;
+    }
+    renderTopics(filteredTopics);
+
+}
+function renderTopics(topicsArray) {
+    topicsArray.forEach(topic => {
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <h3>Category: ${topic.category}</h3>
+            <h3>Topic: ${topic.topic_name}</h3>
+            <h3>Status: ${topic.status}</h3>
+            <h3>Questions Solved: ${topic.questions_solved}</h3>
+
+            <button onClick="editTopic(${topic.id})">Edit</button>
+            <button onClick="deleteTopic(${topic.id})">Delete</button>
+        `;
+
+        topicscontainer.append(div);
+    });
+}
+
+async function getTopics() {
+    const response = await fetch("http://localhost:3000/topics");
+    topics = await response.json();
+
+    populateCategoryFilter();
+
+    if (topics.length === 0) {
+        topicscontainer.innerHTML = `
+        <h2>No topics found.</h2>
+        <p>Click "Add Topic" to create your first topic.</p>
+    `;
+        return;
+    }
+
+    topicscontainer.innerHTML = ""; //to clear the container before fetching new topics
+    console.log(topics);
+    applyFilters();
+
+}
+
+
+function editTopic(id) {
+
+    editTopicId = id;
+    const topic = topics.find(t => t.id == Number(id));
+
+    category.value = topic.category;
+    topicName.value = topic.topic_name;
+    status.value = topic.status;
+    questionsSolved.value = topic.questions_solved;
+    submitBtn.textContent = "Update Topic";
+
+    console.log("Editing topics:", topic);
+}
+
+async function deleteTopic(id) {
+
+    const topic = topics.find(t => t.id == Number(id));
+    const isConfirmed = confirm(`Are you sure you want to delete this topic ${topic.topic_name}?`);
+
+    if (!isConfirmed) {
+        return;
+    }
+
+    await fetch(`http://localhost:3000/topics/${id}`, {
+        method: "DELETE",
+
+    });
+    await getTopics();
+    showMessage("Topic deleted successfully");
+}
+
+async function addTopic(topicData) {
+    const response = await fetch("http://localhost:3000/topics", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(topicData)
+    });
+
+    return response;
+}
+
+async function updateTopic(topicData) {
+    const response = await fetch(`http://localhost:3000/topics/${editTopicId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(topicData)
+    });
+
+    return response;
+}
+
+function resetForm() {
+    form.reset();
+    editTopicId = null;
+    submitBtn.textContent = "Add Topic";
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const topicData = {
+        category: category.value,
+        topicName: topicName.value,
+        status: status.value,
+        questionsSolved: questionsSolved.value
+    };
+
+    if (editTopicId === null) {
+        response = await addTopic(topicData);
+        successMessage = "Topic added successfully!";
+    } else {
+        response = await updateTopic(topicData);
+        successMessage = "Topic updated successfully!";
+    }
+
+    if (response.ok) {
+        await getTopics();
+        resetForm();
+        showMessage(successMessage, "success");
+    } else {
+        showMessage("Operation failed!", "error");
+    }
+
+});
+
+searchInput.addEventListener("input", () => {
+    applyFilters();
+});
+
+categoryFilter.addEventListener("change", () => {
+    applyFilters();
+});
+
+getTopics();
---
+++ server.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "komala13",
+    database: "placement_tracker"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("Database connection failed");
+        console.log(err);
+    } else {
+        console.log("Connected to MySQL successfully");
+    }
+});
+
+
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Placement Preparation Tracker Backend");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This application helps students track placement preparation.");
+});
+
+
+
+
+app.post("/topics", (req, res) => {
+
+    const { category, topicName, status, questionsSolved } = req.body;
+
+    const sqlInsert =
+        "INSERT INTO topics (category, topic_name, status, questions_solved) VALUES (?, ?, ?, ?)";
+
+    db.query(
+        sqlInsert,
+        [category, topicName, status, questionsSolved],
+        function (err, result) {
+
+            if (err) {
+                console.log(err);
+
+                return res.status(500).json({
+                    message: "Failed to add topic"
+                });
+            }
+
+            res.json({
+                message: "Topic added successfully"
+            });
+        }
+    );
+
+});
+app.get("/topics", (req, res) => {
+
+    const sqlSelect = "SELECT * FROM topics";
+
+    db.query(sqlSelect, function (err, result) {
+
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Failed to receive data"
+            })
+        }
+        res.json(result);
+
+
+
+    });
+
+});
+
+app.put("/topics/:id", (req, res) => {
+
+    const id = req.params.id;
+    const { category, topicName, status, questionsSolved } = req.body;
+
+    const sqlupdate = `update topics
+    set 
+         category=?,
+         topic_name=?,
+         status=?,
+         questions_solved=?
+    
+    where id=?
+    `;
+
+    db.query(sqlupdate,
+        [category, topicName, status, questionsSolved, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send("failed to update topic")
+            }
+            if (result.affectedRows === 0) {
+                return res.status(404).json({
+                    message: "Topic not found"
+                });
+            }
+
+            res.json({
+                message: "Topic updated successfully"
+            });
+        }
+    )
+
+})
+
+app.delete("/topics/:id", (req, res) => {
+   
+    const id = req.params.id;
+    const sqldelete = `
+    delete from topics
+    where id=?
+    `;
+
+    db.query(sqldelete, [id], (err, result) => {
+
+        if (err) {
+            return res.status(500).send("Failed to delete");
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: "Topic not found"
+            });
+        }
+
+        res.json({
+            message: "Topic updated successfully"
+        });
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
